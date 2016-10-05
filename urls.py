07d65e15a542fc2cc8f8b568f4ac40d20107970d
davidtnfsh <davidtnfsh@gmail.com>
2016-10-05 09:46:53 +0800

    新增navbar可以到達不同程度規模的地圖圖資

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@
 from django.conf.urls import patterns, url
 import SentiMap
 urlpatterns = patterns('SentiMap.views',
-  url(r'^map/$','map', name='map'),#這樣做似乎是對應到首頁
+  url(r'^map/(?P<MapName>(\w)+)/$','map', name='map'),#這樣做似乎是對應到首頁
 )
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 
-def map(request):
-		return render_to_response('SentiMap/taiwan.html',locals())
+def map(request, MapName):
+	MapName += '.js'
+	return render_to_response('SentiMap/taiwan.html',locals())
