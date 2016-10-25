8eadf8a44698f850347c4f459dc623ad9315eee4
davidtnfsh <davidtnfsh@gmail.com>
2016-10-25 17:44:21 +0800

    修正url寫法到django1.10的語法

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-import SentiMap
-urlpatterns = patterns('SentiMap.views',
-  url(r'^map/(?P<MapName>(\w)+)/$','map', name='map'),#這樣做似乎是對應到首頁
-)
+from SentiMap import views
+urlpatterns = [
+  url(r'^map/(?P<MapName>(\w)+)/$', views.map, name='map'),#這樣做似乎是對應到首頁
+]
